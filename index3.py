a480ec698a98b493a56ad676a8ead53978fc8965
fitz87 <jjoongoo@gmail.com>
2020-08-20 07:27:37 +0900

    del

--- index3.py
+++
@@ -1,7 +0,0 @@
-from google_images_download import google_images_download
-
-response = google_images_download.googleimagesdownload()
-
-arguments = {"keywords":"Polar bears,boloons,Beaches","limie":20,"print_urls":True}
-paths = response.download(arguments)
-print(paths)
