08538bc89d6b43105085b21eb3160675d2f1e4a7
TV 코난쌤 <jjoongoo@gmail.com>
2020-08-19 15:53:33 +0000

    예제추가

--- index.py
+++ index.py
@@ -1,10 +1,14 @@
+#라이블러리를 불러옴
 from bs4 import BeautifulSoup
 from urllib.request import urlopen
 
-response = urlopen('https://www.naver.com/')
-soup = BeautifulSoup(response, 'html.parser')
+#urlopen에서 웹페이지를 불러와서 리스폰스에 넣는다
+response = urlopen('https://www.zum.com/')
+#뷰리플숩이라는 라이블러리를 이용해서 response를 넣어주고 html.parser로 분석한다음 그 결과를 soup에 담음
+soup = BeautifulSoup(response, 'html.parser') 
+
 i = 1
-f = open("새파일.txt", 'w')
+f = open("13.txt", 'w')
 for anchor in soup.select("span.ah_k"):
     data = str(i) + "위 : " + anchor.get_text() + "\n"
     i = i + 1
---
+++ index3.py
@@ -0,0 +1,7 @@
+from google_images_download import google_images_download
+
+response = google_images_download.googleimagesdownload()
+
+arguments = {"keywords":"Polar bears,boloons,Beaches","limie":20,"print_urls":True}
+paths = response.download(arguments)
+print(paths)
